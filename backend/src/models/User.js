10a60404d0cfd7b9f38e6bc7ffe7877ed80c2e17
prashon-10 <prashon109939@gmail.com>
2024-04-03 09:24:06 +0545

    new commit

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,41 +1,56 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        enum: ["male", "female"],
-        required: true,
-    },
-    role:{
-        type: String,
-        enum: ["user", "admin"],
-        default: "user"
-    },
-    image:{
-        type: String
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-    updatedAt:{
-        type: Date,
-        default: Date.now
-    }
-
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export default mongoose.model('User', userSchema);
+userSchema.methods.toJSON = function () {
+  var obj = this.toObject();
+  if(obj.image){
+    obj.image = process.env.PUBLIC_URL + "/users/"+obj.image;
+
+  }
+    else{
+        obj.image = process.env.PUBLIC_URL + "/icon/notfound.png";
+    }
+  
+  delete obj.password;
+  return obj;
+};
+
+export default mongoose.model("User", userSchema);
