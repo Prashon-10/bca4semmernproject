3807ca4f5bf5d087f0193819bdb4fd874ee06352
prashon-10 <prashon109939@gmail.com>
2024-04-04 09:22:23 +0545

    new commit

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,56 +1,55 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  image: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female"],
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    image:{
+        type: String
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now
+    }
+
 });
 
-userSchema.methods.toJSON = function () {
-  var obj = this.toObject();
-  if(obj.image){
-    obj.image = process.env.PUBLIC_URL + "/users/"+obj.image;
-
-  }
-    else{
-        obj.image = process.env.PUBLIC_URL + "/icon/notfound.png";
+userSchema.methods.toJSON = function(){
+    var obj = this.toObject();
+    if(obj.image){
+        obj.image = process.env.PUBLIC_URL + "/users/" + obj.image;
+    }else{
+        obj.image = process.env.PUBLIC_URL + "/icons/notfound.png";
     }
-  
-  delete obj.password;
-  return obj;
-};
+    delete obj.password;
+    return obj;
+}
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model('User', userSchema);
--- backend/src/router/userRoute.js
+++ backend/src/router/userRoute.js
@@ -1,16 +1,15 @@
-import express from "express";
-import UserController from "../controller/UserController.js";
-import UploadMiddleware from "../middleware/UploadMiddleware.js";
-
+import express from 'express';
+import UserController from '../controller/UserController.js';
+import UploadMiddleware from '../middleware/UploadMiddleware.js';
 const userRouter = express.Router();
 const uInstance = new UserController();
 const fInstance = new UploadMiddleware();
-const upload = fInstance.upload("images");
+const upload = fInstance.upload('users');
 
-userRouter.get("/", uInstance.index);
-userRouter.post("/", upload.single("image"), uInstance.store);
-userRouter.get("/:id", uInstance.show);
-userRouter.put("/:id", uInstance.update);
-userRouter.delete("/:id", uInstance.destroy);
+userRouter.get('/', uInstance.index);
+userRouter.post('/',upload.single('image'), uInstance.store);
+userRouter.get('/:id', uInstance.show);
+userRouter.put('/:id', uInstance.update);
+userRouter.delete('/:id', uInstance.destroy);
 
 export default userRouter;
