acc1fce63a174e695046890c6375716d0d9a14cd
prashon-10 <prashon109939@gmail.com>
2024-04-04 09:33:24 +0545

    new commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import Connection from './database/Connection.js';
-import router from './router/web.js';
+import express from "express";
+import Connection from "./database/Connection.js";
+import router from "./router/web.js";
+import cors from "cors";
 const app = express();
 
 new Connection();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(router);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
